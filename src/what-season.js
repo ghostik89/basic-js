6789b600ab241dd68221b276caae3e2edf50fbb2
Lady_Proger <facktartar@mail.com>
2021-03-13 20:55:25 +0300

    slove what season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    if (!date)
+        return 'Unable to determine the time of year!'
+
+  if (date instanceof Date && !isNaN(date.getTime())) {
+        let season = ''
+        let month = date.getMonth() + 1
+        switch (month) {
+            case 12:
+            case 1:
+            case 2:
+                season = 'winter';
+                break;
+            case 3:
+            case 4:
+            case 5:
+                season = 'spring';
+                break;
+            case 6:
+            case 7:
+            case 8:
+                season = 'summer';
+                break;
+            case 9:
+            case 10:
+            case 11:
+                season = 'fall';
+                break;
+        }
+        return season
+    }
+    throw Error
 };
